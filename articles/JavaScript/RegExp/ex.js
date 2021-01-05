d2b64b14682d79afe5dbdab4f496c0c10b94c59a
1099365856@qq.com <1099365856@qq.com>
2021-01-05 22:43:22 +0800

    init and update Javascript RegExp ex.js

---
+++ articles/JavaScript/RegExp/ex.js
@@ -0,0 +1,12 @@
+var re =/ab+c/
+console.log(re)
+var re1= new RegExp("ab+cc")
+console.log(re1)
+//如果正则表达式将会改变，或者它将会从用户输入等来源中动态地产生 : 构造函数
+
+//[\b]匹配一个退格(U+0008)。（不要和\b混淆了。）
+//\b 匹配一个词的边界。
+
+let arr=[1,2,3]
+console.log("-",arr[5],arr[2])//JS数组越界不报错
+
